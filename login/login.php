5a69b8e6aaf4cad7c63d97c1057d4ce095321841
ParzivalPy <gabriel.arthus@icloud.com>
2024-10-17 21:32:47 +0200

    ammélioration de la connexion

--- login/login.php
+++ login/login.php
@@ -26,13 +26,12 @@
                     <label for="email" class="form-label">Email</label>
                     <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help"
                         placeholder="you@exemple.com">
-                    <div id="email-help" class="form-text">L'email utilisé lors de la création de compte.</div>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Mot de passe</label>
                     <input type="password" class="form-control" id="password" name="password">
                 </div>
-                <button type="submit" class="btn btn-primary">Envoyer</button>
+                <input type="submit" id="submit">
             </form>
             <!-- Si utilisateur/trice bien connectée on affiche un message de succès -->
         <?php else: ?>
