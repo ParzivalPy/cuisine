301cdd38e5fb21fffb76f108dbc442c76fb59d02
ParzivalPy <gabriel.arthus@icloud.com>
2024-10-17 21:17:26 +0200

    logout.php

--- login/logout.php
+++ login/logout.php
@@ -2,7 +2,7 @@
 
 session_start(); // Démarrez la session si ce n'est pas déjà fait
 
-require_once(__DIR__ . '/functions.php');
+require_once('../functions.php');
 
 // Détruire la session
 session_unset();
