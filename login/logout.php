53c2cd98bc2319b6cf6dad9c68a978ce1cae457e
ParzivalPy <gabriel.arthus@icloud.com>
2025-01-07 09:23:11 +0100

    Implement cookie management for user login and logout; set cookies on login and clear them on logout

--- login/logout.php
+++ login/logout.php
@@ -4,6 +4,9 @@ session_start(); // Démarrez la session si ce n'est pas déjà fait
 
 require_once('../functions.php');
 
+setcookie("user_id", "", time() - 3600);
+setcookie("password", "", time() - 3600);
+
 // Détruire la session
 session_unset();
 session_destroy();
