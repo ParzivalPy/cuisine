b2a8de4d6cdb78d37a2de0b852047ff162e0b048
ParzivalPy <gabriel.arthus@icloud.com>
2025-01-09 15:46:44 +0100

    Clean up logout process; remove unnecessary comments and ensure proper session handling before redirection

--- login/logout.php
+++ login/logout.php
@@ -1,15 +1,13 @@
 <?php
 
-session_start(); // Démarrez la session si ce n'est pas déjà fait
+session_start();
 
 require_once('../functions.php');
 
 setcookie("email", "", time() - 3600);
 setcookie("password", "", time() - 3600);
 
-// Détruire la session
 session_unset();
 session_destroy();
 
-// Rediriger l'utilisateur vers la page d'accueil
 redirectToUrl('../login/login.php');
